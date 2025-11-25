831f51caadd9eefed92ec91151a98558a1bdfbc3
kotakgirish9@gmail.com <kotakgirish9@gmail.com>
2025-11-25 11:07:47 +0530

    Removed secret from env example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-      themes: ['light', 'dark', 'night', 'cupcake', 'lemonade', 'dracula', 'abyss', 'acid'],
-    },
-  }  
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ['light', 'dark', 'night', 'cupcake', 'lemonade', 'dracula', 'abyss', 'acid'],
+  }
+};
+ 
+  
